25c64adb2b1091d2a0b92c7f766ea5190b10cdc8
sugamocommunistparty <123180385+sugamocommunistparty@users.noreply.github.com>
2023-01-23 20:55:13 +0900

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,9 @@
+function addchange() {
+    if (window.outerWidth < 630) {
+        var AdChange001 = document.getElementById("ad01");
+        AdChange001.innerHTML = '<script src="https://adm.shinobi.jp/s/f5c04764bd4e9eee5ec54fe0e9c6773a"></script><script src="https://adm.shinobi.jp/s/f5c04764bd4e9eee5ec54fe0e9c6773a"></script>' ;
+    }
+}
 function passcheck(){
     if(passwordform.pass.value == "kamikazeattack"){
         location.href='./vip.html';
